134202ea99a0b05276ba6b1a2bc6020ff702956f
PiaMonge <piamonges@gmail.com>
2017-10-05 12:22:24 +0200

    WatchAuth implementation JEE Project

--- JEE/Projet/FrontAuthWatcherWebService/src/main/java/fr/cpe/rest/IWatcherAuth.java
+++ JEE/Projet/FrontAuthWatcherWebService/src/main/java/fr/cpe/rest/IWatcherAuth.java
@@ -1,11 +1,21 @@
 package fr.cpe.rest;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import common.model.UserModel;
 
 @Path ("/WatcherAuth")
 public interface IWatcherAuth {
-//	@POST
-//	@Path("/")
+	
+	//TODO create object containing login and password and send it directly in getUser
+	@POST
+	@Consumes("application/json")
+	@Produces("application/json")
+	@Path("/")
+	UserModel getUser (String login, String password);
 	
 
 }
